ed7469f24a82c573b8553df32a293b9ee8c1c043
Remi1906 <remi93300@hotmail.fr>
2022-02-22 16:50:01 +0100

    + formules et début des unit tests

--- tests/dto/TestGameplay.java
+++ tests/dto/TestGameplay.java
@@ -40,15 +40,6 @@ public class TestGameplay {
 
     @Test
     public void testDamageWithWeaponAdvantage() {
-        CharacterClass charaClass = new CharacterClass();
-        Stats stats = new Stats(10, 10, 2, 1, 1, 1, 10);
-        Weapon wpS = new Weapon(2, false, "arme", WeaponType.SWORD, ElementType.LIGHTNING);
-        Weapon wpA = new Weapon(2, false, "arme", WeaponType.AXE, ElementType.LIGHTNING);
-        Character cS = new Character(0, 0, Direction.SOUTH, "nom", "secondNom", "description", CharacterState.ALIVE, Allegiance.J1, wpS, charaClass, stats);
-        Character cA = new Character(0, 0, Direction.SOUTH, "nom", "secondNom", "description", CharacterState.ALIVE, Allegiance.J1, wpA, charaClass, stats);
-        assertEquals(10, cS.getStats().getHealth());
-        cS.attack(cA);
-        assertEquals(3, cS.damageValue(cA));
     }
 
 
@@ -60,19 +51,6 @@ public class TestGameplay {
 
     @Test
     public void testHeal() {
-        CharacterClass charaClass = new CharacterClass();
-        Stats stats = new Stats(10, 10, 7, 5, 0, 0, 0);
-        Weapon wpNoDmg = new Weapon(1, false, "arme",WeaponType.SWORD, ElementType.LIGHTNING);
-        Character c = new Character(0, 0, Direction.SOUTH, "nom", "secondNom", "description", CharacterState.ALIVE, Allegiance.J1, wpNoDmg, charaClass, stats);
-
-        assertEquals(10, c.getStats().getHealth());
-
-        c.attack(c);
-        assertEquals(3, c.damageValue(c));
-        assertEquals(8, c.getStats().getHealth());
-
-        c.heal(c);
-        assertEquals(9, c.getStats().getHealth());
 
     }
 }
